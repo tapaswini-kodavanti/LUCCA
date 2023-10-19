8785008bb2e6ee5d9c21f6608b53c31c7d1d0c0d
Emerald-Z <ejzhang2004@gmail.com>
2023-10-19 16:13:18 -0500

    redid class

--- fact_retrieval.py
+++ fact_retrieval.py
@@ -8,39 +8,46 @@ from gpt_interface import get_completion
 import os
 import chromadb
 
-embeddings = OpenAIEmbeddings()
-client = chromadb.PersistentClient('chroma_db')
-collection = client.get_or_create_collection("dobby_v1")
-vectorstore = Chroma(
-    client=client,
-    # collection_name="collection_name",
-    embedding_function=embeddings,
-)
+class FactRetrieval:
+    embeddings = OpenAIEmbeddings()
+    client = chromadb.PersistentClient('chroma_db')
+    collection = client.get_or_create_collection("dobby_v1")
+    vectorstore = Chroma(
+        client=client,
+        # collection_name="collection_name",
+        embedding_function=embeddings,
+    )
+    
+    def __init__(self) -> None:
+
+        print("initializing fact retrieval")
+
+    def get_file_names(self):
+        return os.listdir("memory/factual_db")
+
+    def get_file_embeddings(self, filename):
+
+        if filename != "n/a" and self.vectorstore.get(filename)['ids'] == []:
+
+            loader = TextLoader("memory/factual_db/{filename}".format(filename=filename))
+            documents = loader.load()
+
+            text_splitter = CharacterTextSplitter(chunk_size=1000, chunk_overlap=0)
+            documents = text_splitter.split_documents(documents)
+            self.vectorstore.add_documents(documents)
+            # vectorstore = Chroma.from_documents(documents, embeddings, persist_directory="./chroma_db")
+
+    def get_files(self, query):
+        PROMPT="""give me just the document name that will best answer the question: {question}
+                    options: {options}
+                    (if there is no answer, respond with "n/a")
+                """.format(question=query, options=self.get_file_names())
+
+        output = get_completion(PROMPT)
+        print(output)
+        self.get_file_embeddings(output)
+    
 
 # get all file names and ask LLM to choose n best files
 # stick those n files into retrieval chain and completion
 
-def get_file_names():
-    return os.listdir("memory/factual_db")
-
-def get_file_embeddings(filename):
-
-    if filename != "" and vectorstore.get(filename)['ids'] == []:
-
-        loader = TextLoader("memory/factual_db/{filename}".format(filename=filename))
-        documents = loader.load()
-
-        text_splitter = CharacterTextSplitter(chunk_size=1000, chunk_overlap=0)
-        documents = text_splitter.split_documents(documents)
-        vectorstore.add_documents(documents)
-        # vectorstore = Chroma.from_documents(documents, embeddings, persist_directory="./chroma_db")
-
-def get_files(query):
-    PROMPT="""give me just the document name that will best answer the question: {question}
-                options: {options}
-            """.format(question=query, options=get_file_names())
-
-    output = get_completion(PROMPT)
-    print(output)
-    get_file_embeddings(output)
-    
