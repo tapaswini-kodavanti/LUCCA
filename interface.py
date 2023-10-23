14a0e3bf076c80d90818f413426c0961ac484bea
Emerald-Z <ejzhang2004@gmail.com>
2023-10-22 21:36:55 -0500

    saving

--- interface.py
+++ interface.py
@@ -1,17 +1,21 @@
 import os
-os.environ["OPENAI_API_KEY"]="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"
+os.environ["OPENAI_API_KEY"]="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"
 
 from langchain.llms import OpenAI
+from langchain.chat_models import ChatOpenAI
 from langchain.chains import ConversationChain
 from memory_module import memory_exists, memory
 from langchain.chains import ConversationalRetrievalChain
 from langchain.memory import ConversationBufferMemory
-from fact_retrieval import get_files, vectorstore
+# from fact_retrieval import get_files, vectorstore
+from langchain.chains.router import MultiRetrievalQAChain
+from fact_retrieval import FactRetrieval
+
 # first initialize the large language model
-llm = OpenAI(
+llm = ChatOpenAI(
 	temperature=0,
-	openai_api_key="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",
-	model_name="text-davinci-003"
+	openai_api_key="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",
+	model_name="gpt-3.5-turbo-0613"
 )
 
 # take an input
@@ -40,12 +44,32 @@ Chat History:
 Follow Up Input: {question}
 Standalone question:"""
 CONDENSE_QUESTION_PROMPT = PromptTemplate.from_template(_template)
+retrieval = FactRetrieval()
 
 memory = ConversationBufferMemory(memory_key="chat_history", return_messages=True)
-qa = ConversationalRetrievalChain.from_llm(llm, vectorstore.as_retriever(search_kwargs={'k': 3}), condense_question_prompt=CONDENSE_QUESTION_PROMPT, memory=memory)
+qa = ConversationalRetrievalChain.from_llm(llm, retrieval.vectorstore.as_retriever(search_kwargs={'k': 3}), condense_question_prompt=CONDENSE_QUESTION_PROMPT, memory=memory)
 # issue might have to load chroma every time - find a way around this
 chat_history = []
 
+retriever_infos = [
+    {
+        "name": "anna hiss gymnasium",
+        "description": "good for answering ut austin lab related questions",
+        "retriever": retrieval.vectorstore.as_retriever()
+    },
+]
+
+prompt_template = PromptTemplate(
+    input_variables=['query'],
+    template='''
+        You are a helpful assistant named Dobby. Please introduce yourself at the beginning of the conversation, and also ask for the name of the
+        person that you are interacting with. After this, they will give you a question or comment: {query}
+        Respond to this accordingly. 
+    '''
+)
+
+prompt_template.format(query="")
+chain = MultiRetrievalQAChain.from_retrievers(OpenAI(), retriever_infos, verbose=True) # override the prompt and pass memory dict in -> template doesn't work for everything
 def main():
     print(memory_exists)
     while (True):
@@ -57,10 +81,12 @@ def main():
         else:
             # output = conversation.run(query)
 
-            get_files(query)
-            output = qa({"question": query})
+            # get_files(query) # runs to update vector store - slow
+            # output = qa({"question": query})
+            prompt_template.format(query=query)
+            output = chain.run(query)
             # display
-            print(output['answer'])
+            print(output)
 
 if __name__ == "__main__":
     main()
